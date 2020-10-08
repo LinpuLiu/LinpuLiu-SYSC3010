6828d72ad24be8810ba1cf7f100eb2d45d49169a
LinpuLiu <71500435+LinpuLiu@users.noreply.github.com>
2020-10-08 21:49:20 +0800

    [Linpu Liu] feat: this is the commit that I'm using on the lab exam

---
+++ Lab4/Lab4.py
@@ -0,0 +1,19 @@
+# The Hardware I'm using are Raspberry pi and a SenseHat
+from sense_emu import SenseHat
+import time
+
+s = SenseHat()
+# Get the raw information of the compass
+raw = s.get_compass_raw()
+
+# Assign the compass values to x, y, and z
+x = raw["x"]
+y = raw["y"]
+z = raw["z"]
+
+# Dispaly the result on the screen
+s.show_message("coord" + str(int(x)))
+time.sleep(1)
+s.show_message("coord" +str(int(y)))
+time.sleep(1)
+s.show_message("coord" +str(int(z)))
